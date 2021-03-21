013e3bd4d444ea76131be2370d93913ed982cbf8
Yinyin L <yinyinl@gmail.com>
2021-03-21 16:26:44 +0800

    reduce calls by make more assumptions

--- text.go
+++ text.go
@@ -5,19 +5,28 @@ import (
 	"encoding/base64"
 )
 
+const expectTextDataSize = 22
+
+// base64.RawURLEncoding.EncodedLen(md5.Size) == 22
+
 // MarshalText implements the encoding.TextMarshaler interface.
 func (d *MD5Digest) MarshalText() (text []byte, err error) {
 	enc := base64.RawURLEncoding
-	text = make([]byte, enc.EncodedLen(md5.Size))
+	text = make([]byte, expectTextDataSize)
 	enc.Encode(text, d.digest[:])
 	return
 }
 
 // UnmarshalText implements the encoding.TextUnmarshaler interface.
 func (d *MD5Digest) UnmarshalText(text []byte) (err error) {
+	if dlen := len(text); dlen != expectTextDataSize {
+		err = &ErrIncorrectSize{
+			ReceivedBufferSize: dlen,
+		}
+		return
+	}
 	enc := base64.RawURLEncoding
-	dbuf := make([]byte, enc.DecodedLen(len(text)))
-	n, err := enc.Decode(dbuf, []byte(text))
+	n, err := enc.Decode(d.digest[:], text)
 	if nil != err {
 		return
 	}
@@ -27,6 +36,5 @@ func (d *MD5Digest) UnmarshalText(text []byte) (err error) {
 		}
 		return
 	}
-	copy(d.digest[:], dbuf)
 	return
 }
